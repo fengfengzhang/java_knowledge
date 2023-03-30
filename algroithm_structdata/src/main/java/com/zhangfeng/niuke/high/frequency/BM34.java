13ca96cf6661d5747e36ebdc5f51db2d96f4c485
fengfengzhang <1459387877@qq.com>
2023-03-30 18:42:40 +0800

    判断是否为二叉搜索树

--- algroithm_structdata/src/main/java/com/zhangfeng/niuke/high/frequency/BM34.java
+++ algroithm_structdata/src/main/java/com/zhangfeng/niuke/high/frequency/BM34.java
@@ -8,8 +8,7 @@ package com.zhangfeng.niuke.high.frequency;
  */
 public class BM34 {
     /**
-     * 输入一棵节点数为 n 二叉树，判断该二叉树是否是平衡二叉树。
-     * 在这里，我们只需要考虑其平衡性，不需要考虑其是不是排序二叉树
+     * 判断是否为二叉搜索树
      */
 
     public static class TreeNode {

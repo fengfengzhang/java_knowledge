48d409b06d5f279de2ffc6dfa123c5902a42e3fc
张峰 <w_zhangfeng2@xiwang.com>
2025-05-30 17:18:14 +0800

    最大子数组和

--- algroithm_structdata/src/main/java/com/zhangfeng/leetcode/LeetCode53.java
+++ algroithm_structdata/src/main/java/com/zhangfeng/leetcode/LeetCode53.java
@@ -1,90 +1,32 @@
 package com.zhangfeng.leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-
 public class LeetCode53 {
+
     /**
-     * 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。请你合并所有重叠的区间，并返回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间 。
+     * 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
      *
-     *
-     *
-     * 示例 1：
-     *
-     * 输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
-     * 输出：[[1,6],[8,10],[15,18]]
-     * 解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
-     * 示例 2：
-     *
-     * 输入：intervals = [[1,4],[4,5]]
-     * 输出：[[1,5]]
-     * 解释：区间 [1,4] 和 [4,5] 可被视为重叠区间。
+     * 子数组是数组中的一个连续部分。
+     * @param nums
+     * @return
      */
 
-    public class Node {
-        int start;
-        int end;
+    public int maxSubArray(int[] nums) {
+        int cur = 0;
+        int max = 0;
+        for(int i = 0; i < nums.length ; i++){
+            cur += nums[i];
 
-        public Node(int start , int end){
-            this.start = start;
-            this.end = end;
-        }
-    }
+            if(i == 0){
+                max = cur;
+            }else{
+                max = Math.max(cur,max);
+            }
 
-    public int[][] merge(int[][] intervals) {
-       if(intervals == null || intervals.length == 0 || intervals[0] == null || intervals[0].length == 0){
-           return new int[][]{};
-       }
-
-       PriorityQueue<Node> queue = new PriorityQueue<>(
-               (i,j) ->{
-                   if(i.start < j.start){
-                       return -1;
-                   }else if(i.start > j.start){
-                       return 1;
-                   }else{
-                       return i.end < j.end ? -1 : 1;
-                   }
-               }
-       );
-
-       ArrayList<Node> list = new ArrayList<>();
-
-        for (int[] interval : intervals) {
-            queue.add(new Node(interval[0], interval[1]));
+            if(cur < 0){
+                cur = 0;
+            }
         }
 
-       while (!queue.isEmpty()){
-           Node node = queue.poll();
-           if(list.isEmpty()){
-               list.add(node);
-           }else{
-               Node preNode = list.get(list.size() - 1);
-               if(preNode.start <= node.start && preNode.end >= node.start){
-                   preNode.end = Math.max(node.end,preNode.end);
-               }else{
-                   list.add(node);
-               }
-           }
-       }
-
-       int[][] res = new int[list.size()][2];
-       for(int i = 0; i < list.size() ; i++){
-           res[i][0] = list.get(i).start;
-           res[i][1] = list.get(i).end;
-       }
-
-       return res;
-
+        return max;
     }
-
-    public static void main(String[] args) {
-        int[][] merge = new LeetCode53().merge(new int[][]{{1,3},{2,6},{8,10},{15,18}});
-        for(int[] a : merge){
-            System.out.println(Arrays.toString(a));
-        }
-
-    }
-
 }

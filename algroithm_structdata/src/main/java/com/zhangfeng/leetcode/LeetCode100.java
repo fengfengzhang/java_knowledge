613bbc0eb18569588ec3e2c6e099611bcf1d22aa
w_zhangfeng2 <w_zhangfeng2@xiwang.com>
2025-12-18 18:35:56 +0800

    fixed

--- algroithm_structdata/src/main/java/com/zhangfeng/leetcode/LeetCode100.java
+++ algroithm_structdata/src/main/java/com/zhangfeng/leetcode/LeetCode100.java
@@ -6,7 +6,7 @@ package com.zhangfeng.leetcode;
  * @Author zhangfeng
  * @Date 2025/12/16 11:29
  */
-public class LeeCode100 {
+public class LeetCode100 {
     /**
      * 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
      */
@@ -43,7 +43,7 @@ public class LeeCode100 {
     }
 
     public static void main(String[] args) {
-        LeeCode100 leeCode100 = new LeeCode100();
+        LeetCode100 leeCode100 = new LeetCode100();
         System.out.println(leeCode100.trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1}));
     }
 

f8f405e849fede218327ffb492859a97c7a8f5ff
w_zhangfeng2 <w_zhangfeng2@xiwang.com>
2026-01-05 17:24:04 +0800

    买卖股票最佳时机

---
+++ algroithm_structdata/src/main/java/com/zhangfeng/leetcode/LeetCode122.java
@@ -0,0 +1,44 @@
+package com.zhangfeng.leetcode;
+
+/**
+ * @ClassName LeetCode122
+ * @Description TODO
+ * @Author zhangfeng
+ * @Date 2026/1/5 17:23
+ */
+public class LeetCode122 {
+    /**
+     * 给你一个整数数组 prices ，其中 prices[i] 表示某支股票第 i 天的价格。
+     *
+     * 在每一天，你可以决定是否购买和/或出售股票。你在任何时候 最多 只能持有 一股 股票。然而，你可以在 同一天 多次买卖该股票，但要确保你持有的股票不超过一股。
+     *
+     * 返回 你能获得的 最大 利润 。
+     * 输入：prices = [7,1,5,3,6,4]
+     * 输出：7
+     * 解释：在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5 - 1 = 4。
+     * 随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 这笔交易所能获得利润 = 6 - 3 = 3。
+     * 最大总利润为 4 + 3 = 7 。
+     */
+
+    public int maxProfit(int[] prices) {
+        if(prices == null || prices.length == 0){
+            return 0;
+        }
+
+
+        int[][] dp = new int[2][prices.length];
+
+        dp[0][0] = -prices[0];
+        dp[1][0] = 0;
+
+        for(int i = 1; i< prices.length ; i++){
+            dp[0][i] = Math.max(dp[0][i-1],dp[1][i-1] - prices[i]);
+            dp[1][i] = Math.max(dp[1][i-1],dp[0][i-1] + prices[i]);
+        }
+
+        return dp[1][prices.length - 1];
+
+
+    }
+
+}
